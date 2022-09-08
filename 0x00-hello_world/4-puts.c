e8a6890cdf92c957f05e28abe73ca139d574715a
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-09 00:08:53 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
 * main - using puts to print
 * programming is like building a multilingual puzzle
-* Description: my first C P.L code, aimed at printing 
+* Description:my first C P.L code, aimed at printing 
 * Programming is like building a multilingual puzzle
 * with the puts function
-* Return: Always 0 (success)
+* Return:Always 0 (success)
 */
 int main(void)
 {
