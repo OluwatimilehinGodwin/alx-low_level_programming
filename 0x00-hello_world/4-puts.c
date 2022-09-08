2d95e8b7d7a571c3c08587002f37375364f54328
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-08 23:48:53 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - using puts to print
- * proghamming is like building a multilingual puzzle
- *
- * Description: my first C P.L code, aimed at printing 
- * Programming is like building a multilingual puzzle
- * with the puts function
- * Return: Always 0 (success)
- */
- int main(void)
+* main - using puts to print
+* proghamming is like building a multilingual puzzle
+*
+* Description: my first C P.L code, aimed at printing 
+* Programming is like building a multilingual puzzle
+* with the puts function
+* Return: Always 0 (success)
+*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
