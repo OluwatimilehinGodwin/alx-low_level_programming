a12df2c41276a9194d738e8727992a9972a30c9b
OluwatimilehinGodwin <olubogodwin93@gmail.com>
2022-09-08 18:26:15 -0700

    6-sizeghy

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,14 +11,12 @@ int main(void)
 	char c;
 	int i;
 	long l;
-	long long xl;
+	long long ll;
 	float f;
-
 	printf("size of a char: %ld byte(s)\n", sizeof(c));
 	printf("size of an int: %ld byte(s)\n", sizeof(i));
 	printf("size of a long int: %ld byte(s)\n", sizeof(l));
-	printf("size of a long long int: %ld byte(s)\n", sizeof(xl));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(ll));
 	printf("size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
-
