679ba8a6e48d7f18a79d075bf3baaebdd1a763d6
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-09 21:34:27 +0100

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: program to print the alphabeth in small letters
+ * Return: 0
+ */
+int main(void)
+{
+char m = "a"
+
+while (m <= "z")
+{
+	putchar(m);
+	m++;
+}
+return (0);
+}
