7f58037f89371ad16acb4f7acfd13df2e03d4d29
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-11 13:44:18 +0100

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Discription: and that piece of art is useful
+ * " - Dora Korpar, 2015-10-19"
+ * Return: 0
+ */
+int main(void)
+{
+char st[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+fprintf("%s\n", st);
+return(0);
+}
