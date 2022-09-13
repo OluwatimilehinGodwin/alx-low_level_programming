76f4effa81b0bc1305a7bcf237ff8ff5310c4d59
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-13 21:38:30 +0100

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,18 +16,18 @@ if (n > '0')
 	_putchar(' ');
 	return (1);
 }
-else if (n = '0')
-{
-	_putchar('0');
-	_putchar(',');
-	_putchar(' ');
-	return (0);
-}
-else
+else if (n < '0')
 {
 	_putchar('-');
 	_putchar(',');
 	_putchar(' ');
 	return (-1);
+}
+else
+{
+	_putchar('0');
+	_putchar(',');
+	_putchar(' ');
+	return (0);
 }
 }
