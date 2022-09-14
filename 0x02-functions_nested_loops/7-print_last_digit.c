935079319af28ea5f1daed9b74cd46edd35ecf90
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-14 23:28:48 +0100

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,6 +2,7 @@
 
 /**
  * print_last_digit
+ * @d: active variable
  *
  * Description: a function that prints the last digit
  * of a number
@@ -10,7 +11,11 @@
 int print_last_digit(int d)
 {
 int last_digits = d % 10;
-
+	
+if (d < 0)
+{
+	d = d * -1;
+}
 	_putchar(last_digits);
 return (last_digits);
 }
