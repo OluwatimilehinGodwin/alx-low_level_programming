e679c8882b132f8db5370c20b29370ea1e4e40ec
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-15 00:11:06 +0100

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - entry point
+ * @a: active variable
+ * @b: second active variable
+ *
+ * Description: function that adds two integers
+ * and returns the result
+ * Return: 0
+ */
+int add(int a, int b);
+{
+int result = a + b;
+
+return (result);
+}
