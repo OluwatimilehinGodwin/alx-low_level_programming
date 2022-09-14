02c9a192292bf74fb2a7215c6985fc21622bc3b3
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-14 23:52:58 +0100

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,11 +2,19 @@
 
 /**
  * _abs - entry point
- *
+ *@a: active variable
  * Description: function that computes the absolute
  * value of an integer.
  * Return: 0
  */
-int _abs(int)
+int _abs(int a)
 {
-
+if (a >= 0)
+{
+	return(a);
+}
+else if (a < 0)
+{
+	return (a * -1);
+}
+}
