98ad0693f35b0e537b27205522c63c10300759a7
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-15 01:06:08 +0100

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,25 +11,22 @@
  */
 void print_to_98(int n)
 {
-if (n < 98)
+if (n <= 98)
 {
 	while (n < 98)
 	{
 		printf("%i, ", n);
 		n++;
 	}
+	printf("%i\n", n);
 }
-else if (n > 98)
+else
 {
 	while (n > 98)
 	{
 		printf("%i, ", n);
 		n--;
 	}
+	printf("%i\n", n);
 }
-else
-{
-	_putchar(n);
-}
-_putchar('\n');
 }
