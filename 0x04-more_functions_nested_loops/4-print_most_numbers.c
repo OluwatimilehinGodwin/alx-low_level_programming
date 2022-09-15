9411dbdcb4f0c4d0faf1b5765643b751a3b83159
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-15 22:16:02 +0100

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - entry point
+ *
+ * Description: print the 0 to 9 number,
+ * exempting 2 and 4
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+int j = '0';
+
+while (j <= '9')
+{
+	if (j != '2' || j != '4')
+	{
+		_putchar(j);
+		j++;
+	}
+}
+_putchar('\n');
+}
