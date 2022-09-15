e8eb67610797656a5df58fc74638c6e2a58cfa3f
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-16 00:01:16 +0100

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_square - entry point
+ * @size: active variable
+ *
+ * Description: printing a square
+ * Return: 0
+ */
+void print_square(int size)
+{
+if (size > '0')
+{
+	while (size > '0')
+	{
+
