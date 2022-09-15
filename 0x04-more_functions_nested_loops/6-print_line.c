71228c5a98113137f4143cbbb20d167e0ca7c298
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-15 22:57:23 +0100

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - entry point
+ * @n: active variable
+ *
+ * Description: function that draws a straight
+ * line in the terminal.
+ * Return: 0
+ */
+void print_line(int n)
+{
+if (n > '0')
+{
+	while (n > '0')
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\\n');
+}
+else
+{
+	_putchar('\\n');
+}
+}
