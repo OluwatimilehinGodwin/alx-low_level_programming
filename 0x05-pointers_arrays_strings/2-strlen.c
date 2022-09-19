3ad23735cf8c753a88144de02d4fececbf5d1628
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-19 17:20:44 +0100

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - entry point
+ * @s: active variable
+ *
+ * Description: function that returns the length of a string.
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+int x = (*s / 8);
+
+return (x);
+}
