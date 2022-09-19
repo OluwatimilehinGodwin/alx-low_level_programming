272832d3c61f7138e7312897357983352c18b1f9
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-20 00:18:02 +0100

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - entry point
+ * @str: active variable
+ *
+ * Description: function that prints every other
+ * character of a string, starting with the first character,
+ * Return: 0
+ */
+void puts2(char *str)
+{
+int count = 0;
+
+while (*(str + count) != '\0')
+{
+	_putchar(count);
+	count++;
+}
+_putchar('\n');
+}
