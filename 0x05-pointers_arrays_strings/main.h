3ea02305b7cbe51e5d2afb13f14eb6d30168f38b
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-20 13:47:13 +0100

    3-puts.c5-rev_string.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
 int _putchar(char tr);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -6,3 +9,5 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+
+#endif
