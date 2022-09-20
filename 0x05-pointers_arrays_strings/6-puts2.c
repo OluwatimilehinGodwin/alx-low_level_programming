8e9149c96799141331fd9893c27149428f63072f
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-20 15:11:56 +0100

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,12 +14,11 @@ int count = 0;
 
 while (*(str + count) != '\0')
 {
-	_putchar(*(str + count));
-	if (*(str + count) = "\\")
+	if (*(str + count) != "\\")
 	{
-		break;
+		_putchar(*(str + count));
+		count = count + 2;
 	}
-	count = count + 2;
 }
 _putchar('\n');
 }
