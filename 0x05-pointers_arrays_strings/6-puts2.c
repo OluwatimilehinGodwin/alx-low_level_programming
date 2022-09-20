0fb38cb072eb9f25431fe8f2ca01fbe7fe341fef
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-20 01:14:01 +0100

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,10 +12,10 @@ void puts2(char *str)
 {
 int count = 0;
 
-while (*(str + count) != '\0')
+while (*(str + count) != '\0' || *(str + count) != '\')
 {
-		_putchar(*(str + count));
-		count = count + 2;
+	_putchar(*(str + count));
+	count = count + 2;
 }
 _putchar('\n');
 }
