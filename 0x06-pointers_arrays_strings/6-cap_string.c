607082b2afeefa3d45921e6f700d172a5b6d55b9
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-23 00:18:46 +0100

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * cap_string - entry point
+ * @s: active variable
+ *
+ * Description: a function to capitalize all inputs
+ * Return: 0
+ */
+char *cap_string(char *s)
+{
+int c = 0;
+
+while (s[c] != '\0')
+{
+	if (s[c] >= 97 && s[c] <= 122)
+	{
+		s[c] -= 32;
+		c++;
+	}
+	else
+	{
+		c++;
+	}
+}
+return (s);
+};
