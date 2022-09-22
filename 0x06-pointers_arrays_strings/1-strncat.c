c3cc12461d838356c245d0f821d8cdbbad545844
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-22 21:28:54 +0100

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,14 +13,29 @@ char *_strncat(char *dest, char *src, int n)
 {
 int c = 0;
 int c2;
+int c3 = 0;
 
 while (dest[c] != '\0')
 {
 	c++;
 }
-for (c2 = 0; c2 < n; c2++)
+while (src[c3] != '\0')
 {
-	dest[c] = src[c2];
+	c3++;
+}
+if (n <= c3)
+{
+	for (c2 = 0; c2 < n; c2++)
+	{
+		dest[(c + c2)] = src[c2];
+	}
+}
+else
+{
+	for (c2 = 0; c2 < c3; c2++)
+	{
+		dest[(c + c2)] = src[c2];
+	}
 }
 return (dest);
 }
