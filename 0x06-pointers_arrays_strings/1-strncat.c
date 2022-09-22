0207aee873f9b2efdba860ee775b00dc1c1d6f2c
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-22 14:39:55 +0100

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - entry point
+ * @dest: active variable
+ * @src: another active variable
+ * @n: yet another active variable
+ *
+ * Description: function to append n bytes of src int dest
+ * Return: 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int c = 0;
+int c2 = 0;
+
+while (dest[c] != '\0')
+{
+	c++;
+}
+while (c2 < n)
+{
+	dest[c] = src[c2];
+	c2++;
+
