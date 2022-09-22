5b2b856fe20a5cc5e9ef97f446f86b32aec217cb
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-22 16:25:20 +0100

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,7 @@ int _strcmp(char *s1, char *s2)
 int i;
 int flag = 0;
 
-for (i = 0; s1[i] != '\0' || s2[i] != 0;)
+for (i = 0; s1[i] != '\0' || s2[i] != 0; i++)
 {
 	if (s1[i] !=  s2[i])
 	{
@@ -24,8 +24,6 @@ for (i = 0; s1[i] != '\0' || s2[i] != 0;)
 	{
 		continue;
 	}
-	i++;
-
 }
 return (flag);
 }
