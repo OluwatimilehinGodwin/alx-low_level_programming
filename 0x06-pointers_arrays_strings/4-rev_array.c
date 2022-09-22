771e4aa0490dc04c29d9dc3fb24732c198b6d145
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-22 22:06:34 +0100

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - entry point
+ * @a: active variable
+ * @n: second active variable
+ * 
+ * Description: a function that reverses the content of an array
+ * Return: 0
+ */
+void reverse_array(int *a, int n)
+{
+char bk;
+int c = 0;
+
+while (c < n/2)
+{
+	bk = *(a + c);
+	a[c] = a[(n - 1 - c)];
+	a[(n-1)] = bk;
+}
+}
