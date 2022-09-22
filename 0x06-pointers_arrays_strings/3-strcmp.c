07915e4564203fc2c5409e93de477ddd7479b2db
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-22 16:19:41 +0100

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcmp - entry point
+ * @s1: active variable
+ * @s2: sevond avtive variable
+ *
+ * Description: a function to compare two strings
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i;
+int flag = 0;
+
+for (i = 0; s1[i] != '\0' || s2[i] != 0;)
+{
+	if (s1[i] !=  s2[i])
+	{
+		flag = (s1[i] - s2[i]);
+		break;
+	}
+	else
+	{
+		continue;
+	}
+	i++;
+
+}
+return (flag);
+}
