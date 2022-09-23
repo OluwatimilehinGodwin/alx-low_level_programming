85d6c97e2c5a6c65c445c3092d3329dd1c571d9b
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-23 02:41:47 +0100

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - entry point
+ * @p: active variable
+ *
+ * Description: function to convert certain alphabeth to numbers
+ * Return: 0
+ */
+char *leet(char *p)
+{
+int q, r;
+char l[] = "aAeEoOtTsS";
+char m[] = "4433007711";
+
+for (q = 0; *(p + q) != '\0'; q++)
+{
+	for (r = 0; r < 10; r++)
+	{
+		if (*(p + q) == l[r])
+		{
+			*(p + q) = m[r];
+		}
+	}
+}
+return (p);
+}
