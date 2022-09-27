8d37e72bc5907efeabf66bd2e8821efa8b50566e
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-27 23:47:29 +0100

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - entry point
+ * @s: active var
+ * @to: 2nd var
+ *
+ * Description: set value of pointer to character
+ * Return: 0
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
