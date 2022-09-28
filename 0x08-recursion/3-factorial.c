98da61b272dba96695adebfbbb54dd26418fe019
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-28 17:22:55 +0100

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - entry point
+ * @n: active string variable
+ *
+ * Description: a function to find the factorial of a number
+ * Return: 0
+ */
+int factorial(int n)
+{
+int x;
+
+if (n = 1)
+{
+	return (1);
+}
+else
+{
+	return (n * factorial(int n);
+}
+}
