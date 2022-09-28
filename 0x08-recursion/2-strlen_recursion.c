6d9a0d084f771f9e3a4d9b4d68e92f489bc0b6d3
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-28 17:13:08 +0100

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,14 +11,10 @@ int _strlen_recursion(char *s)
 {
 int i = 0;
 
-if (*s == '\0')
+if (s[0] != '\0')
 {
-	return (i);
+	i += 1;
+	i += _strlen_recursion(s + 1);
 }
-else
-{
-	i++;
-	_strlen_recursion(s + 1);
-}
-return (0);
+return (i);
 }
