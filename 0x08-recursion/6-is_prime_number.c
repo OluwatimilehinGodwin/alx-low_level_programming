edd1860b187ee39ec57100d19a7c2b6cc0fac157
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-28 20:24:22 +0100

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * is_prime_number - entry point
+ * @n: active variable
+ *
+ * Description: a function to check if number is prime
+ * Return: 0
+ */
+int is_prime_number(int n)
+{
+if ((n % 2) == 1 && (n % 3) > 0 && (n % 5) > 0 && (n % 9) > 0)
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
