9486fdb5bc2cace57686cab52c5a6e694e4ef4d2
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-29 20:56:39 +0100

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,7 +5,7 @@
  * @x: active variable
  * @y: second active variable
  *
- * Description: function that calculates a x to the power of y 
+ * Description: a function that calculates a x to the power of y
  * Return: 0
  */
 int _pow_recursion(int x, int y)
