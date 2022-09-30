be3f09ca325e13faf08098c3b0a0d66520d2f191
Oluwatimilehin <olubogodwin93@gmail.com>
2022-09-30 09:25:27 +0100

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: count of C.L. argument
+ * @argv: pointer to C.L. array of argument
+ *
+ * Description: a function to print the name of the file
+ * Return: 0
+ */
+int main(int argc, char** argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+a directory containing solutions to the alx 0x0A-argc_argv task	
