2de2c0251191cf382a59bcc8813b84abed560864
Oluwatimilehin <olubogodwin93@gmail.com>
2022-10-04 11:42:04 +0100

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,7 +5,7 @@
 /**
  * create_array - entry point
  * @size: size of array to be created
- * c: initialization character
+ * @c: initialization character
  *
  * Description: a function to create and initialize a character
  * Return: pointer to the character or NULL
@@ -13,13 +13,17 @@
 char *create_array(unsigned int size, char c)
 {
 char *point;
+unsigned int i;
 
 if (size == 0)
 {
 	return (NULL);
 }
 point = malloc(size * sizeof(char));
-point[0] = c;
+for (i = 0; i < size; i ++)
+{
+	point[i] = c;
+}
 if (point != NULL)
 {
 	return (point);
