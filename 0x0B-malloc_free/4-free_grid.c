bd3355ff86c42ffb5485f40fedea0d1514175e2b
OluwatimilehinGodwin <olubogodwin93@gmail.com>
2022-10-05 13:33:29 -0700

    free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - entry point
+ * @grid: 2-d array passed
+ * @height: row
+ *
+ * Description: a function to free a 2-d grid 
+ * Return: freed grid
+ */
+void free_grid(int **grid, int height)
+{
+int i;
+
+for (i = 0; i < height; i++)
+{
+	free(grid[i]);
+}
+free(grid);
+}
