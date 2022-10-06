8508238c5ab68039dc44c3cf6e9be54c4fba3d0c
Oluwatimilehin <olubogodwin93@gmail.com>
2022-10-06 22:28:31 +0100

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,12 +19,12 @@ if (nmemb == 0 || size == 0)
 {
 	return (NULL);
 }
-array = malloc(nmemb * size);
+array = malloc((nmemb * size));
 if (array == NULL)
 {
 	return (NULL);
 }
-for (i = 0; i < nmemb; i++)
+for (i = 0; i < (nmemb * size); i++)
 {
 	array[i] = 0;
 }
