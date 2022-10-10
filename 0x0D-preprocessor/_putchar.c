29686f656bf44f188539388383469fe1e4a7c9e4
OluwatimilehinGodwin <olubogodwin93@gmail.com>
2022-10-10 14:07:55 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/_putchar.c
@@ -0,0 +1,12 @@
+nclude <stdio.h>
+
+/**
+ * _putchar - entry point
+ * Description: prints the character p to the stdout
+ * Return: 0
+ */
+int _putchar(char tr)
+{
+	putchar(tr);
+	return (0);
+}
