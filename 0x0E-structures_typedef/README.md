1c4389d9e6a0c0830ae7e01a94d5fa9ba89ddeec
Oluwatimilehin <olubogodwin93@gmail.com>
2022-10-11 15:29:32 +0100

    dog-header

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+a dirextory containing answers to the alx 0x0E-structures_typedef tasks
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG
+#define DOG
+
+#include <stdio.h>
+
+/**
+ * struct dog - dog structure
+ * @name: name of dog
+ * @age: dogs age
+ * @owner: dogs owner
+ *
+ * Description: dog structure header file
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
