84baf2aa5d253bda126f6e75ba38e3020e87be59
OluwatimilehinGodwin <olubogodwin93@gmail.com>
2022-10-11 14:18:01 -0700

    new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,29 +1,51 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
- * new_dog - entry point
+ * new_dog - creates a new dog
  * @name: name of dog
  * @age: age of dog
- * @owner: dog's owner
+ * @owner: owner of dog
  *
- * Description: a function to creat a new dog structure
+ * Return: pointer to new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *new_struct;
-int count, i, j;
+int count, count2, i;
+dog_t *new;
 
-if (name == NULL || age < 0 || owner == NULL)
+if (name == NULL || owner == NULL || age < 0)
 	return (NULL);
-new_struct = malloc(sizeof(dog_t));
-if (new_struct == NULL)
-{
+new = malloc(sizeof(dog_t));
+if (new == NULL)
 	return (NULL);
-for(count = 0; name[i]; i++)
+for (count = 0; name[count]; count++)
 	;
-i++;
-new_struct->name = malloc(i * sizeof(char));
-if (new_struct->name == NULL)
+count++;
+new->name = malloc(count * sizeof(char));
+if (new->name == NULL)
+{
+	free(new);
 	return (NULL);
-for (i = 0; i <
+}
+for (i = 0; i < count; i++)
+{
+	new->name[i] = name[i];
+}
+new->age = age;
+for (count2 = 0; owner[count2]; count2++)
+	;
+count2++;
+new->owner = malloc(count2 * sizeof(char));
+if (new->owner == NULL)
+{
+	free(new->name);
+	free(new);
+	return (NULL);
+}
+for (i = 0; i < count2; i++)
+{
+	new->owner[i] = owner[i];
+}
+return (new);
+}
