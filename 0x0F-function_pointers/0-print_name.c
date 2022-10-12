b651eb111214f6ac0a0456f48dd280bd1315ff98
OluwatimilehinGodwin <olubogodwin93@gmail.com>
2022-10-12 14:29:39 -0700

    print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - entry point
+ * @name: name to ne printed
+ * @f: function to print name
+ *
+ * Description: a function to print the name passed into it
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+a directory that contains answers to the alx 0x0F-function_pointerstasks
